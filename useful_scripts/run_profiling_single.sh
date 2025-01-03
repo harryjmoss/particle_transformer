45bc5d6e80a52edd1b7fa2ca803fdfa7096181fb
Harry Moss <h.moss@ucl.ac.uk>
2025-01-03 16:53:57 +0000

    Rename obtain_dataloader to profiling_steerer

--- useful_scripts/run_profiling_single.sh
+++ useful_scripts/run_profiling_single.sh
@@ -3,7 +3,7 @@
 batch_sizes=(2) 
 for batch_size in "${batch_sizes[@]}"; do
   echo "Running profiling with batch size $batch_size..."
-  python useful_scripts/obtain_dataloader.py --batch_size "$batch_size" --backward
+  python useful_scripts/profiling_steerer.py --batch_size "$batch_size" --backward
 done
 
 echo "All runs completed."
--- useful_scripts/run_timings.sh
+++ useful_scripts/run_timings.sh
@@ -4,7 +4,7 @@ batch_sizes=(2 4 8 16 32 64 128 256 512)
 
 for batch_size in "${batch_sizes[@]}"; do
 	echo "Running timing (no profiler scaffoling) with batch size $batch_size..."
-  python useful_scripts/obtain_dataloader.py --batch_size "$batch_size" --timer
+  python useful_scripts/profiling_steerer.py --batch_size "$batch_size" --timer
 done
 
 echo "All runs completed."
